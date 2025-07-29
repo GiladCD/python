76edc4d04cab21f753fdeca4fe8365ab47ae15b1
GiladCD <giladcd@gmail.com>
2025-07-29 18:38:37 +0300

    thread

---
+++ Thread.py
@@ -0,0 +1,27 @@
+import threading
+
+count = 0
+lock = threading.Lock()
+
+def thread1():
+    global count
+    for i in range(100000):
+        with lock:
+            count += 1
+            print(count)
+
+def thread2():
+    global count
+    for i in range(100000):
+        with lock:
+            count -= 1
+            print(count)
+
+thr1 = threading.Thread(target=thread1)
+thr2 = threading.Thread(target=thread2)
+
+thr1.start()
+thr2.start()
+
+thr1.join()
+thr2.join()
